c25f48282884fa8ec1259c9fe8a86473cb045d3d
DEVCSPWRLD <86549072+DEVCSPWRLD@users.noreply.github.com>
2021-07-08 21:31:10 +0530

    Update wish.java

--- src/main/java/com/cen/wish/wish.java
+++ src/main/java/com/cen/wish/wish.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class wish {
 
-	@RequestMapping
+	@GetMapping("/myfriendrahul")
 	public String wish1() {
 		return "wish";
 		
